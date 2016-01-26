b3036bdff5ab8e8ece36154a7ab553c4b225f785
phileverton <phil@inclind.com>
2016-01-26 16:36:21 -0500

    Created Gruntfile and tested shopify connection

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,8 @@ module.exports = function (grunt) {
   grunt.initConfig({
     shopify: {
       options: {
-        api_key: "fe9cdc30048dd28e9f9f7823c3d2a961",
-        password: "37e6bbafb58dfa5f0ab4352cfa34025d",
+        api_key: "",
+        password: "",
         url: "historic-lewes.myshopify.com",
         base: "shop/"
       }
